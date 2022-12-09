3e654a61a6765f413420659d744e3383be75e5fa
aokenu <austineokenu@gmail.com>
2022-12-08 17:41:06 -0800

    update C script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-/*This script prints "Programming is like building a multilingual puzzle*/
+/**
+ * main - Entrry point
+ *
+ * Description: This script prints "Programming is like building a multilingual puzzle*
+ *
+ * Return: Always 0 (Success)
+ * /
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
