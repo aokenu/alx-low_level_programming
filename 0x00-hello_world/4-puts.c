a90b74a7c048252b8cd8386461f594a548ac1414
aokenu <austineokenu@gmail.com>
2022-12-08 17:37:09 -0800

    A C script that prints out a sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*This script prints "Programming is like building a multilingual puzzle*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
