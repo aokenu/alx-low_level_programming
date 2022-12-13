b556aa5d7b04e454ba49cbb50c03299f6edbd8c2
aokenu <austineokenu@gmail.com>
2022-12-12 16:55:25 -0800

    using putchat to print all possible different combinations of 3-digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of thre digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i, j, k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			k = j + 1;
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(44);
+					putchat(32);
+				}
+				k++;
+			}
+			j++;
+		}
+		i++
+	}
+
+	putchar(10);
+
+	return (0);
+}
