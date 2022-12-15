00cd7ba0d0e9ed5fa7c6f50b54a0506dc9523c8e
aokenu <austineokenu@gmail.com>
2022-12-15 11:05:37 -0800

    -using _putchar to pront 10 time all alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
