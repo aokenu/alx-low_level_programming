dd3c93297a41b6a427d288ee952f068c6bf450fe
aokenu <austineokenu@gmail.com>
2022-12-15 03:36:27 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	char text[10] = "_putchar";
-	int i = 0;
+	char str[10] = "_putchar";
+	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(text[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 
