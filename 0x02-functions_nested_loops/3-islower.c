2e489f69ad90e4d085be0a72a59757548022a8ad
aokenu <austineokenu@gmail.com>
2022-12-15 17:22:55 -0800

    addedd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-*_islower - check if an alphabet is in lowercase
-*
-*parameter or member 'c' - any letter pass in for check
-*
-*return value of '_islower' - 0 (uppercase) 1(lowercase);
-*/
-
+ *_islower - check if an alphabet is in lowercase
+ *
+ *parameter or member 'c'- anything
+ *
+ *Return: return value of '_islower' - 0 (uppercase) 1(lowercase);
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
