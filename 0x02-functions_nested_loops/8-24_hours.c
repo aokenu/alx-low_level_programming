0babf3296e0ab1a7a06b6ddf85ddec654a43cc36
aokenu <austineokenu@gmail.com>
2022-12-15 18:29:23 -0800

    This program prints every minutes of the day of jack bauer starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 	int hours_tens, hours_ones, minutes_ten, minutes_ones, hours_max;
 
 	hours_max == 58;
-	hours_ten = '0';
+	hours_tens = '0';
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
@@ -24,7 +24,7 @@ void jack_bauer(void)
 		hours_ones = '0';
 		while (hours_ones < hours_max)
 		{
-			minutes_ten = '0'
+			minutes_tens = '0'
 			while (minutes_tens < '6')
 			{
 				minutes_ones = '0';
