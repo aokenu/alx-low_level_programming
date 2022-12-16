cb4ab2b1c52eacd53a61d5e177a483863e2f01cd
aokenu <austineokenu@gmail.com>
2022-12-15 17:53:23 -0800

    program checks for alphabetic character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_islower - check if an alphabet is in lowercase
- *
- *parameter or member 'c'- anything
- *
- *Return: return value of '_islower' - 0 (uppercase) 1(lowercase);
- */
+*_islower - check if an alphabet is in lowercase
+*parameter or member 'c' - parameter or member 'c'
+*
+*Return: return value of '_islower' - 0 (uppercase) 1(lowercase);
+*/
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
